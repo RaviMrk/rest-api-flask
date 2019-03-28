aa10625fbb0aa043e752e418e0b37454b37348fe
RaviMrk <raviparmar12316@gmail.com>
2019-03-28 22:56:04 +0530

    changes in user registration file

--- models/user.py
+++ models/user.py
@@ -5,21 +5,43 @@ class UserModel(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
+    email = db.Column(db.String(120))
     password = db.Column(db.String(80))
+    cast = db.Column(db.String(80))
+    fname = db.Column(db.String(80))
+    lname = db.Column(db.String(80))
+    gender = db.Column(db.String(80))
+    university = db.Column(db.String(80))
+    tfws = db.Column(db.String(80))
+    defence = db.Column(db.String(80))
+    department = db.Column(db.String(80))
 
-    def __init__(self, username, password):
-        self.username = username
+
+
+    def __init__(self, email, password, cast, fname, lname, gender, university, tfws, defence, department):
+        self.email = email
         self.password = password
+        self.cast = cast
+        self.fname = fname
+        self.lname = lname
+        self.gender = gender
+        self.university = university
+        self.tfws = tfws
+        self.defence = defence
+        self.department = department
+
+    def json(self):
+        return {'email': self.email,'password':self.password, 'cast': self.cast, 'fname': self.fname, 'lname': self.lname, 'gender': self.gender, 'university': self.university, 'tfws': self.tfws, 'defence' : self.defence, 'department': self.department }
 
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-    def find_by_username(cls, username):
-        return cls.query.filter_by(username=username).first()
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
 
     @classmethod
     def find_by_id(cls, _id):
         return cls.query.filter_by(id=_id).first()
+
--- resources/user.py
+++ resources/user.py
@@ -5,7 +5,7 @@ from models.user import UserModel
 class UserRegister(Resource):
 
     parser = reqparse.RequestParser()
-    parser.add_argument('username',
+    parser.add_argument('email',
         type=str,
         required=True,
         help="This field cannot be blank."
@@ -15,14 +15,69 @@ class UserRegister(Resource):
         required=True,
         help="This field cannot be blank."
     )
+   
+    parser.add_argument('cast',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
+
+    parser.add_argument('fname',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
+
+    parser.add_argument('lname',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
+
+    parser.add_argument('gender',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
+
+    parser.add_argument('university',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
+
+    parser.add_argument('tfws',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
+
+    parser.add_argument('defence',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
+
+    parser.add_argument('department',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
+
+
 
     def post(self):
         data = UserRegister.parser.parse_args()
 
-        if UserModel.find_by_username(data['username']):
-            return {"message": "A user with that username already exists"}, 400
+        if UserModel.find_by_email(data['email']):
+            return {"message": "A user with that email already exists"}, 400
 
         user = UserModel(**data)
         user.save_to_db()
 
         return {"message": "User created successfully."}, 201
+
+class UserList(Resource):
+    def get(self):
+        return {"user":[x.json() for x in UserModel.query.all()]}
+        
